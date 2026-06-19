717c1292f9922a4ba3388e67c38837e22842b107
GabrielRamosGomes <gabrielramosgomes82@gmail.com>
2026-06-19 21:35:06 +0100

    changed hardcoded local ip to variable

--- scripts/mount-nas.sh
+++ scripts/mount-nas.sh
@@ -11,13 +11,13 @@ fi
 
 source "$ENV_FILE"
 
-mountpoint -q /mnt/nas/filebrowser || mount -t nfs 192.168.1.102:/volume1/filebrowser /mnt/nas/filebrowser \
+mountpoint -q /mnt/nas/filebrowser || mount -t nfs $NAS_IP:/volume1/filebrowser /mnt/nas/filebrowser \
   -o rw,nfsvers=3,proto=tcp,_netdev,noatime,hard,timeo=600,retrans=2
 
-mountpoint -q /mnt/nas/immich || mount -t nfs 192.168.1.102:/volume1/immich /mnt/nas/immich \
+mountpoint -q /mnt/nas/immich || mount -t nfs $NAS_IP:/volume1/immich /mnt/nas/immich \
   -o rw,nfsvers=3,proto=tcp,_netdev,noatime,hard,timeo=600,retrans=2
 
-mountpoint -q /mnt/nas/jellyfin || mount -t nfs 192.168.1.102:/volume1/jellyfin /mnt/nas/jellyfin \
+mountpoint -q /mnt/nas/jellyfin || mount -t nfs $NAS_IP:/volume1/jellyfin /mnt/nas/jellyfin \
   -o rw,nfsvers=3,proto=tcp,_netdev,noatime,hard,timeo=600,retrans=2
 
 echo "NAS shares mounted successfully."
