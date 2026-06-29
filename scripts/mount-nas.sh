2a1cc0303550c1d84367f19ab7410424265e3c87
GabrielRamosGomes <gabrielramosgomes82@gmail.com>
2026-06-29 19:41:06 +0100

    updated scripts + jellyfin compose

--- scripts/mount-nas.sh
+++ scripts/mount-nas.sh
@@ -11,13 +11,13 @@ fi
 
 source "$ENV_FILE"
 
-mountpoint -q /mnt/nas/filebrowser || mount -t nfs $NAS_IP:/volume1/filebrowser /mnt/nas/filebrowser \
+mountpoint -q /mnt/nas/filebrowser || sudo mount -t nfs $NAS_IP:/volume1/filebrowser /mnt/nas/filebrowser \
   -o rw,nfsvers=3,proto=tcp,_netdev,noatime,hard,timeo=600,retrans=2
 
-mountpoint -q /mnt/nas/immich || mount -t nfs $NAS_IP:/volume1/immich /mnt/nas/immich \
+mountpoint -q /mnt/nas/immich || sudo mount -t nfs $NAS_IP:/volume1/immich /mnt/nas/immich \
   -o rw,nfsvers=3,proto=tcp,_netdev,noatime,hard,timeo=600,retrans=2
 
-mountpoint -q /mnt/nas/jellyfin || mount -t nfs $NAS_IP:/volume1/jellyfin /mnt/nas/jellyfin \
+mountpoint -q /mnt/nas/jellyfin || sudo mount -t nfs $NAS_IP:/volume1/jellyfin /mnt/nas/jellyfin \
   -o rw,nfsvers=3,proto=tcp,_netdev,noatime,hard,timeo=600,retrans=2
 
 echo "NAS shares mounted successfully."
